b9115c53f774ee7114ce01dd7f849cecd8ce3c69
aBram <eyalandron@gmail.com>
2022-12-08 01:29:49 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# to_ascii
+
+Commandline tool used to convert images into ascii images. Mostly just an excuse to try rust.
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::io::Write;
+
+
+
+/// Simple program to greet a person
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Source image path
+   #[arg(short, long)]
+   src: String,
+
+   /// Path to save the ascii image to
+   #[arg(short, long)]
+   dst: String,
+
+}
+
+const GRAYSCALE: &str = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
+fn main() -> std::io::Result<()> {
+    let args = <Args as clap::Parser>::parse();        
+
+
+    let img = image::io::Reader::open(&args.src)?
+                            .with_guessed_format()?
+                            .decode()
+                            .expect("Failed to decode image");
+    let mut img = img.grayscale();
+    let img = img
+                                                .as_mut_luma8()
+                                                .unwrap();
+
+    let mut ascii_output = std::fs::File::create(args.dst).expect("destination already exists!");
+
+
+    for x in 0..img.width()
+    {
+        for y in 0..img.height()
+        {
+            let pixel = img.get_pixel(x, y);
+            let char = GRAYSCALE.as_bytes()[usize::from(pixel.0[0]) * GRAYSCALE.len() / 255] as char;
+            ascii_output.write(format!("{}", char).as_str().as_bytes()).unwrap();
+        }
+        ascii_output.write(b"\n").unwrap();
+    }
+
+    return Ok(());
+}
